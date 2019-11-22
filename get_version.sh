837d0fc856db54087d24540d48e505becb2ae5d1
Oliver Fassbender <git@intrepid.de>
2019-11-22 18:59:29 +0000

    added parameter support in Jenkinsfile and action.sh

--- get_version.sh
+++ get_version.sh
@@ -2,6 +2,6 @@
 
 #https://www.privoxy.org/
 #<p>The most recent release is <a href="announce.txt" target="_top">3.0.28 (stable)</a>.</p>
-VERSION=`curl -s "https://www.privoxy.org/" | grep 'The most recent release is ' | sed -n -e s~'^.*<a href="announce.txt" target="_top">'~~p | sed -n -e s~' (stable)</a>.</p>.*$'~~p | sed -e s~'\r'~~`
+VERSION=`curl -s "https://www.privoxy.org/" | grep 'The most recent release is ' | sed -n -e s~'^.*<a href="announce.txt" target="_top">'~~p | sed -n -e s~' (stable)</a>.</p>.*$'~~p`
 
 echo ${VERSION}
