886dc2368e9f52a7e1945071eba391afbdcc49c3
Oliver Faßbender <git@intrepid.de>
2021-02-12 06:35:22 +0000

    changed build process

--- get_version.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#https://www.privoxy.org/
-#<p>The most recent release is <a href="announce.txt" target="_top">3.0.28 (stable)</a>.</p>
-VERSION=`curl -s "https://www.privoxy.org/" | grep 'The most recent release is ' | sed -n -e s~'^.*<a href="announce.txt" target="_top">'~~p | sed -n -e s~' (stable)</a>.</p>.*$'~~p`
-
-echo ${VERSION}
